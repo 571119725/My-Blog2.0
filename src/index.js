f2a223240c1d9cac2c28ceae3dc0fea07c03c0b5
caozheng <571119725@qq.com>
2022-05-08 13:54:11 +0800

    ss

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TopNavigation from '@/layout/js/TopNavigation'
+import LeftPart from '@/layout/js/LeftPart'
+import RightPart from '@/layout/js/RightPart'
+import MainContent from '@/layout/js/MainContent'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <TopNavigation />
+    <div>
+      <LeftPart />
+      <MainContent />
+      <RightPart />
+    </div>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
